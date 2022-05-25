65e0fb6b5b5fabd1a5f297cf2fddcdfbc173c5f0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-05-24 22:03:08 -0500

    clase 5

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,14 +1,24 @@
 import { FlatList, Text } from "react-native";
-import data from '../utils/data';
 import Video from './Video';
+import { useState, useEffect } from 'react';
 
 const Videos = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+            fetch('https://api.mercadolibre.com/sites/MLA/search?q=squidgame')
+                .then(res => res.json())
+                .then(result => setData(result.results))
+                .catch(err => console.log(err));
+    }, []);
+
     return (
         <FlatList
             data={data}
             ItemSeparatorComponent={() => <Text></Text>}
             renderItem={({item}) => (
-                <Video {...item} />
+                // <Video {...item} />
+                <Text>{item.title}</Text>
             )}
         />
     );
